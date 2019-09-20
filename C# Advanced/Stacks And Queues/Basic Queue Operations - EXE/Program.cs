3e9b1ec0d86e33c625d910ae4a3effde9943e015
NikolayStefanov <54640586+NikolayStefanov@users.noreply.github.com>
2019-09-20 10:26:53 +0300

    Add files via upload

--- C# Advanced/Stacks And Queues/Basic Queue Operations - EXE/Program.cs
+++ C# Advanced/Stacks And Queues/Basic Queue Operations - EXE/Program.cs
@@ -1,1 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace _2._Basic_Queue_Operations__EXE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var NSX = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int numbersToEnqueue = NSX[0];
+            int numbersToDequeue = NSX[1];
+            int searchingNumber = NSX[2];
+
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var theQueue = new Queue<int>();
+            foreach (var number in numbers)
+            {
+                theQueue.Enqueue(number);
+            }
+            if (numbersToDequeue >= theQueue.Count)
+            {
+                theQueue.Clear();
+                Console.WriteLine(0);
+                return;
+            }
+            for (int i = 0; i < numbersToDequeue; i++)
+            {
+                theQueue.Dequeue();
+            }
+            int smallestNum = int.MaxValue;
+            if (theQueue.Contains(searchingNumber))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                foreach (var item in theQueue)
+                {
+                    if (item < smallestNum)
+                    {
+                        smallestNum = item;
+                    }
+                }
+                Console.WriteLine(smallestNum);
+            }
+
+        }
+    }
+}
