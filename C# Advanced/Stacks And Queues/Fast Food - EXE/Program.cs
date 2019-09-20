3ec2008cb88cb53ce6e469cb9ca9bb3bd5d07dcc
NikolayStefanov <54640586+NikolayStefanov@users.noreply.github.com>
2019-09-20 10:29:32 +0300

    Add files via upload

--- C# Advanced/Stacks And Queues/Fast Food - EXE/Program.cs
+++ C# Advanced/Stacks And Queues/Fast Food - EXE/Program.cs
@@ -1,1 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace _4._Fast_Food__REAL_EXE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodForDay = int.Parse(Console.ReadLine());
+            var inputOrders = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var queue = new Queue<int>(inputOrders);
+            Console.WriteLine(queue.Max());
+            while (queue.Any())
+            {
+                if (foodForDay - queue.Peek() >= 0)
+                {
+                    foodForDay -= queue.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine($"Orders left: " + string.Join(" ", queue));
+                    return;
+                }
+            }
+            Console.WriteLine("Orders complete");
+        }
+    }
+}
