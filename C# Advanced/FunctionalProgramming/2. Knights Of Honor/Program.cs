1d075518b4bbc65ae467da82c35fc206674ff4cf
NikolayStefanov <54640586+NikolayStefanov@users.noreply.github.com>
2019-10-09 20:30:49 +0300

    Add files via upload

--- C# Advanced/FunctionalProgramming/2. Knights Of Honor/Program.cs
+++ C# Advanced/FunctionalProgramming/2. Knights Of Honor/Program.cs
@@ -1,1 +1,21 @@
+﻿using System;
 
+namespace _2._Knights_of_Honor_EXE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var allNames = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries);
+            Action<string[]> sirInserter = PrintNames;
+            sirInserter(allNames);
+        }
+        public static void PrintNames(string[] names)
+        {
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Sir {name}");
+            }
+        }
+    }
+}
