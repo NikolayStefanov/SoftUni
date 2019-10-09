36240c4d0ec7b600bfeb42f421d9f13a529cd9a9
NikolayStefanov <54640586+NikolayStefanov@users.noreply.github.com>
2019-10-09 20:37:22 +0300

    Add files via upload

--- C# Advanced/FunctionalProgramming/8.Custum Comparator/Program.cs
+++ C# Advanced/FunctionalProgramming/8.Custum Comparator/Program.cs
@@ -1,1 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace _8._Custom_Comparator_EXE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Func<int[], List<int>> sortedNumbers = EvenOddComparator;
+            Console.WriteLine(string.Join(' ', sortedNumbers(numbers)));
+
+
+        }
+        public static List<int> EvenOddComparator(int[] numbers)
+        {
+            var sortedNums = new List<int>();
+            var evenNums = new List<int>();
+            var oddNums = new List<int>();
+            foreach (var num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    evenNums.Add(num);
+                }
+                else
+                {
+                    oddNums.Add(num);
+                }
+            }
+            evenNums.Sort();
+            oddNums.Sort();
+            sortedNums.AddRange(evenNums);
+            sortedNums.AddRange(oddNums);
+
+            return sortedNums;
+        }
+    }
+}
