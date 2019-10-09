5b71739d94abffa8e57b04ce6b94d59884d7efaa
NikolayStefanov <54640586+NikolayStefanov@users.noreply.github.com>
2019-10-09 20:36:31 +0300

    Add files via upload

--- C# Advanced/FunctionalProgramming/7.Predicate For Names/Program.cs
+++ C# Advanced/FunctionalProgramming/7.Predicate For Names/Program.cs
@@ -1,1 +1,23 @@
+﻿using System;
+using System.Linq;
 
+namespace _7._Predicate_for_Names_EXE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nameLenght = int.Parse(Console.ReadLine());
+            var names = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            PrintRightNames(nameLenght, names);
+        }
+
+        private static void PrintRightNames(int nameLenght, string[] names)
+        {
+            foreach (var name in names.Where(x => x.Length <= nameLenght))
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
