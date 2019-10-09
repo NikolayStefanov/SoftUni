2f6814fd3f2f3ed909975f4a9c75f6af98ce8c9b
NikolayStefanov <54640586+NikolayStefanov@users.noreply.github.com>
2019-10-09 20:32:19 +0300

    Add files via upload

--- C# Advanced/FunctionalProgramming/3.Custum Min Function/Program.cs
+++ C# Advanced/FunctionalProgramming/3.Custum Min Function/Program.cs
@@ -1,1 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace _3._Custom_Min_Function_EXE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputNums = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            HashSet<int> numbers = new HashSet<int>(inputNums);
+
+            Func<HashSet<int>, int> minNumber = MinNumber;
+            Console.WriteLine(minNumber(numbers));
+        }
+        public static int MinNumber(HashSet<int> numbers)
+        {
+            var minNum = int.MaxValue;
+            foreach (var num in numbers)
+            {
+                if (num < minNum)
+                {
+                    minNum = num;
+                }
+            }
+            return minNum;
+        }
+    }
+}
