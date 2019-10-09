bf7fc65df1a0c0e9ade9791fe42b254b8f796ddd
NikolayStefanov <54640586+NikolayStefanov@users.noreply.github.com>
2019-10-09 20:38:49 +0300

    Add files via upload

--- C# Advanced/FunctionalProgramming/9.List Of Predicates/Program.cs
+++ C# Advanced/FunctionalProgramming/9.List Of Predicates/Program.cs
@@ -1,1 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace _9._List_of_Predicates_EXE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            var sequenceOfDividers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            var rightNumbers = new List<int>();
+            Func<List<int>, int, bool> isDivide = IsDivided;
+            for (int i = 1; i <= number; i++)
+            {
+                if (isDivide(sequenceOfDividers, i))
+                {
+                    rightNumbers.Add(i);
+                }
+            }
+            Console.WriteLine(string.Join(' ', rightNumbers));
+
+        }
+        public static bool IsDivided(List<int> numbers, int currNum)
+        {
+            foreach (var num in numbers)
+            {
+                if (currNum % num != 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
