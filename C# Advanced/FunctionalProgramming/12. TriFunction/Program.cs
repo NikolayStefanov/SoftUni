fb0ee9cc2adec8b1135e1e67e863352f7b1985a8
NikolayStefanov <54640586+NikolayStefanov@users.noreply.github.com>
2019-10-09 20:41:16 +0300

    Add files via upload

--- C# Advanced/FunctionalProgramming/12. TriFunction/Program.cs
+++ C# Advanced/FunctionalProgramming/12. TriFunction/Program.cs
@@ -1,1 +1,18 @@
+﻿using System;
+using System.Linq;
 
+namespace _12._TriFunction_EXE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int, bool> isLargerOrEqual = (word, criteria) => word.Sum(x => x) >= criteria;
+            int targerSum = int.Parse(Console.ReadLine());
+            string[] namesInput = Console.ReadLine().Split();
+            Func<string[], Func<string, int, bool>, string> myFunc = (names, isLarger) => names.FirstOrDefault(x => isLarger(x, targerSum));
+            string targetName = myFunc(namesInput, isLargerOrEqual);
+            Console.WriteLine(targetName);
+        }
+    }
+}
