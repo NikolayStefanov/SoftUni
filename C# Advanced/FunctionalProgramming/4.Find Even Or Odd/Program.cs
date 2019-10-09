643cb30248092692450500236ad6e4671197a96e
NikolayStefanov <54640586+NikolayStefanov@users.noreply.github.com>
2019-10-09 20:33:30 +0300

    Add files via upload

--- C# Advanced/FunctionalProgramming/4.Find Even Or Odd/Program.cs
+++ C# Advanced/FunctionalProgramming/4.Find Even Or Odd/Program.cs
@@ -1,1 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace _4._Find_Evens_or_Odds_exe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var range = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var evenOrOdd = Console.ReadLine().ToLower();
+
+            Predicate<int> isEven = IsEven;
+            var finalList = new List<int>();
+            if (evenOrOdd == "even")
+            {
+                for (int i = range[0]; i <= range[1] ; i++)
+                {
+                    if (isEven(i))
+                    {
+                        finalList.Add(i);
+                    }
+                }
+            }
+            else if (evenOrOdd == "odd")
+            {
+                for (int i = range[0]; i <= range[1]; i++)
+                {
+                    if (!IsEven(i))
+                    {
+                        finalList.Add(i);
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(' ', finalList));
+
+        }
+        private static bool IsEven(int number) => number % 2 == 0;
+    }
+}
