80e9335ea84ffbf1e191d4a12d745b26331e1276
NikolayStefanov <54640586+NikolayStefanov@users.noreply.github.com>
2019-10-09 20:34:27 +0300

    Add files via upload

--- C# Advanced/FunctionalProgramming/5.Applied Arithmetics/Program.cs
+++ C# Advanced/FunctionalProgramming/5.Applied Arithmetics/Program.cs
@@ -1,1 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace _5._Applied_Arithmetics_EXE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            Func<string, List<int>, List<int>> simpleOperator = Operations;
+            var com = string.Empty;
+            while ((com = Console.ReadLine()) != "end")
+            {
+                if (com == "print")
+                {
+                    PrintTheList(numbers);
+                }
+                else
+                {
+                    simpleOperator(com, numbers);
+                }
+            }
+
+        }
+
+        private static void PrintTheList(List<int> numbers)
+        {
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        public static List<int> Operations(string command, List<int> numbers)
+        {
+            switch (command)
+            {
+                case "add":
+                    for (int i = 0; i < numbers.Count; i++)
+                    {
+                        numbers[i] += 1;
+                    }
+                    break;
+                case "multiply":
+                    for (int i = 0; i < numbers.Count; i++)
+                    {
+                        numbers[i] *= 2;
+                    }
+                    break;
+                case "subtract":
+                    for (int i = 0; i < numbers.Count; i++)
+                    {
+                        numbers[i] -= 1;
+                    }
+                    break;
+            }
+            return numbers;
+        }
+    }
+}
