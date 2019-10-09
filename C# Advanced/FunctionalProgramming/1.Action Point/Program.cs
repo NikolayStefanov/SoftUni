4c3c5551cfe63a23af7626a507a344b563c6d11a
NikolayStefanov <54640586+NikolayStefanov@users.noreply.github.com>
2019-10-09 20:29:31 +0300

    Add files via upload

--- C# Advanced/FunctionalProgramming/1.Action Point/Program.cs
+++ C# Advanced/FunctionalProgramming/1.Action Point/Program.cs
@@ -1,1 +1,22 @@
+﻿using System;
 
+namespace _1._Action_Point_exe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var allNames = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Action<string[]> namesAction = PrintNames;
+            namesAction(allNames);
+        }
+
+        private static void PrintNames(string[] names)
+        {
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
