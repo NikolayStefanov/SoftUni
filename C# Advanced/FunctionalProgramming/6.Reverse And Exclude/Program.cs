54bb1a3d3bf5b8ccf7687f46c5564517de5ba93b
NikolayStefanov <54640586+NikolayStefanov@users.noreply.github.com>
2019-10-09 20:35:25 +0300

    Add files via upload

--- C# Advanced/FunctionalProgramming/6.Reverse And Exclude/Program.cs
+++ C# Advanced/FunctionalProgramming/6.Reverse And Exclude/Program.cs
@@ -1,1 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace _6._Reverse_and_Exclude_ЕXE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int divisionNum = int.Parse(Console.ReadLine());
+            var newOrder = numbers.Reverse().Where(x=> x % divisionNum != 0);
+            Console.WriteLine(string.Join(" ", newOrder));
+        }
+    }
+}
