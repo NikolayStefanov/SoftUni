b61483c5dec0f3118d8349afbd238d87a1b0a5bd
NikolayStefanov <54640586+NikolayStefanov@users.noreply.github.com>
2019-10-14 09:56:24 +0300

    Add files via upload

--- C# Advanced/Sets And Advanced Dictionaries/2.Set of Elements/Program.cs
+++ C# Advanced/Sets And Advanced Dictionaries/2.Set of Elements/Program.cs
@@ -1,1 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace _2._Sets_of_Elements_EXE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstandSecondSet = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var firstSet = new HashSet<int>();
+            var secondSet = new HashSet<int>();
+
+            for (int i = 0; i < firstandSecondSet[0]; i++)
+            {
+                var firstInput = int.Parse(Console.ReadLine());
+                firstSet.Add(firstInput);
+            }
+            for (int i = 0; i < firstandSecondSet[1]; i++)
+            {
+                var secondInput = int.Parse(Console.ReadLine());
+                secondSet.Add(secondInput);
+            }
+            var listOfSameNums = new List<int>();
+            foreach (var num in firstSet)
+            {
+                foreach (var secNum in secondSet)
+                {
+                    if (num == secNum)
+                    {
+                        listOfSameNums.Add(num);
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ", listOfSameNums));    
+        }
+    }
+}
