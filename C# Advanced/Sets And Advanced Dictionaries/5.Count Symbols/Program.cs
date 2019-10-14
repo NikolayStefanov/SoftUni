2bb4df3b79b20a24b5f11253150b03570843c310
NikolayStefanov <54640586+NikolayStefanov@users.noreply.github.com>
2019-10-14 09:58:53 +0300

    Add files via upload

--- C# Advanced/Sets And Advanced Dictionaries/5.Count Symbols/Program.cs
+++ C# Advanced/Sets And Advanced Dictionaries/5.Count Symbols/Program.cs
@@ -1,1 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace _5._Count_Symbols_EXE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var theDict = new SortedDictionary<char, int>();
+            string inputText = Console.ReadLine();
+            for (int i = 0; i < inputText.Length; i++)
+            {
+                var currChar = inputText[i];
+                if (!theDict.ContainsKey(currChar))
+                {
+                    theDict[currChar] = 0;
+                }
+                theDict[currChar]++;
+            }
+
+            foreach (var currSymbol in theDict)
+            {
+                Console.WriteLine($"{currSymbol.Key}: {currSymbol.Value} time/s");
+            }
+        }
+    }
+}
