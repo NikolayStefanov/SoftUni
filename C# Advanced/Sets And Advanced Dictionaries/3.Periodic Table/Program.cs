dacd6828858877fbd07d85d7048f88ce9461aa94
NikolayStefanov <54640586+NikolayStefanov@users.noreply.github.com>
2019-10-14 09:57:13 +0300

    Add files via upload

--- C# Advanced/Sets And Advanced Dictionaries/3.Periodic Table/Program.cs
+++ C# Advanced/Sets And Advanced Dictionaries/3.Periodic Table/Program.cs
@@ -1,1 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
 
+namespace _3._Periodic_Table_EXE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var hashSet = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var inputElements = Console.ReadLine().Split();
+                for (int j = 0; j < inputElements.Length; j++)
+                {
+                    hashSet.Add(inputElements[j]);
+                }
+            }
+            Console.WriteLine(string.Join(" ", hashSet));
+        }
+    }
+}
