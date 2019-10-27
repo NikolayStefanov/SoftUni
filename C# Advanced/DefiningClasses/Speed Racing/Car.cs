518d26865ea0b22037ca0fb4f11166da52d84382
NikolayStefanov <54640586+NikolayStefanov@users.noreply.github.com>
2019-10-27 23:18:51 +0200

    Add files via upload

---
+++ C# Advanced/DefiningClasses/Speed Racing/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpeedRacing
+{
+    public class Car
+    {
+        public Car(string model, double fuelAmount, double fuelConsumption)
+        {
+            this.Model = model;
+            this.FuelAmount = fuelAmount;
+            this.FuelConsumptionPerKilometer = fuelConsumption;
+        }
+        public string Model { get; set; }
+        public double FuelAmount { get; set; }
+        public double FuelConsumptionPerKilometer { get; set; }
+        public double TravelledDistance { get; private set; } = 0;
+
+        public bool Move(string carModel, double amountOfKm)
+        {
+            var isEnough = amountOfKm * FuelConsumptionPerKilometer;
+            if (FuelAmount - isEnough > 0)
+            {
+                FuelAmount -= isEnough;
+                TravelledDistance += amountOfKm;
+            }
+            else
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
--- C# Advanced/DefiningClasses/Speed Racing/Program.cs
+++ C# Advanced/DefiningClasses/Speed Racing/Program.cs
@@ -1,1 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace SpeedRacing
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> carTrack = new List<Car>();
+            int numberOfCars = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                var fillUpCarTrack = Console.ReadLine().Split();
+                var model = fillUpCarTrack[0];
+                var fuelAmount = double.Parse(fillUpCarTrack[1]);
+                var fuelConsumptionPerKm = double.Parse(fillUpCarTrack[2]);
+                var sameModel = false;
+                foreach (Car car in carTrack)
+                {
+                    if (car.Model == model)
+                    {
+                        sameModel = true;
+                        break;
+                    }
+                }
+                if (!sameModel)
+                {
+                    var currCar = new Car(model, fuelAmount, fuelConsumptionPerKm);
+                    carTrack.Add(currCar);
+                }
+            }
+            var command = string.Empty;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                var commandInList = command.Split();
+                var carModel = commandInList[1];
+                var kilometers = double.Parse(commandInList[2]);
+                var isEnough = true;
+
+                foreach (Car car in carTrack)
+                {
+                    if (car.Model == carModel)
+                    {
+                        isEnough = car.Move(carModel, kilometers);
+                        break;
+                    }
+                }
+                if (!isEnough)
+                {
+                    Console.WriteLine("Insufficient fuel for the drive");
+                }
+
+            }
+            foreach (Car car in carTrack)
+            {
+                Console.WriteLine($"{car.Model} {car.FuelAmount:f2} {car.TravelledDistance}");
+            }
+        }
+    }
+}
