5c73d08244d739e29d8fec6a222e1542b63d5eec
NikolayStefanov <54640586+NikolayStefanov@users.noreply.github.com>
2019-10-27 23:28:29 +0200

    Add files via upload

---
+++ C# Advanced/Generics/Generic Box Of String/Box.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1._Generic_Box_of_String
+{
+    public class Box<T>
+    {
+        private T value;
+
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+        public T Value
+        {
+            get { return value; }
+            set { this.value = value; }
+        }
+        public override string ToString()
+        {
+            var type = Value.GetType();
+            var result = $"{type}: {Value}";
+            return result;
+        }
+    }
+}
--- C# Advanced/Generics/Generic Box Of String/StartUp.cs
+++ C# Advanced/Generics/Generic Box Of String/StartUp.cs
@@ -1,1 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
 
+namespace _1._Generic_Box_of_String
+{
+    public class StartUp  
+    {
+        static void Main(string[] args)
+        {
+            var theBoxList = new List<Box<int>>();
+            var inputLines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < inputLines; i++)
+            {
+                var input =int.Parse(Console.ReadLine());
+                var box = new Box<int>(input);
+                theBoxList.Add(box);
+            }
+            foreach (var box in theBoxList)
+            {
+                Console.WriteLine(box.ToString());
+            }
+
+        }
+    }
+}
