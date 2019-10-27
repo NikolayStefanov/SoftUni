80b52b9757d77ac417536011bc465642d420dd52
NikolayStefanov <54640586+NikolayStefanov@users.noreply.github.com>
2019-10-27 23:30:37 +0200

    Add files via upload

---
+++ C# Advanced/Generics/Generic Count Method With Strings/Comparer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _5.GenericCountMethodStrings
+{
+    public class Comparer<T>
+        where T : IComparable
+    {
+        private List<T> list;
+        private T element;
+        public Comparer(List<T> theList,  T element)
+        {
+            this.List = theList;
+            this.element = element;
+
+        }
+        public List<T> List
+        {
+            get { return list; }
+            set { list = value; }
+        }
+
+        public int LargerElementsByComparedElement()
+        {
+            var count = 0;
+            foreach (var item in List)
+            {
+                if (item.CompareTo(element) > 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+    }
+}
--- C# Advanced/Generics/Generic Count Method With Strings/StartUp.cs
+++ C# Advanced/Generics/Generic Count Method With Strings/StartUp.cs
@@ -1,1 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
 
+namespace _5.GenericCountMethodStrings
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var currList = new List<double>();
+            for (int i = 0; i < lines; i++)
+            {
+                var input =double.Parse(Console.ReadLine());
+                currList.Add(input);
+            }
+            var theElement =double.Parse(Console.ReadLine());
+            var compare = new Comparer<double>(currList, theElement);
+            Console.WriteLine(compare.LargerElementsByComparedElement());
+        }
+    }
+}
