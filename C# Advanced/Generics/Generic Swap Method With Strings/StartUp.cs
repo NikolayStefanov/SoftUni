67e71ca1645a94d0566db498797648ae689a4b61
NikolayStefanov <54640586+NikolayStefanov@users.noreply.github.com>
2019-10-27 23:29:32 +0200

    Add files via upload

--- C# Advanced/Generics/Generic Swap Method With Strings/StartUp.cs
+++ C# Advanced/Generics/Generic Swap Method With Strings/StartUp.cs
@@ -1,1 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace _3.GenericSwapMethodStrings
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var list = new List<int>();
+            for (int i = 0; i < lines; i++)
+            {
+                var input = int.Parse(Console.ReadLine());
+                list.Add(input);
+            }
+            var theSwapper = new Swapper<int>(list);
+            var theIndexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            theSwapper.SwapIndexes(theIndexes[0], theIndexes[1]);
+            var finalList = theSwapper.TheList.ToArray();
+            foreach (var item in finalList)
+            {
+                var type = item.GetType();
+                Console.WriteLine($"{type}: {item}");
+            }
+
+
+        }
+    }
+}
---
+++ C# Advanced/Generics/Generic Swap Method With Strings/Swapper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3.GenericSwapMethodStrings
+{
+    public class Swapper<T>
+    {
+        private List<T> theList;
+
+        public Swapper(List<T> list)
+        {
+            this.TheList = new List<T>();
+            this.TheList = list;
+        }
+        public List<T> TheList
+        {
+            get { return theList; }
+            set { theList = value; }
+        }
+        public void SwapIndexes(int firstIndex, int secondIndex)
+        {
+            if (firstIndex >=0 && firstIndex < theList.Count && secondIndex >= 0 && secondIndex < theList.Count && firstIndex != secondIndex)
+            {
+                var firstValue = TheList[firstIndex];
+                TheList[firstIndex] = TheList[secondIndex];
+                TheList[secondIndex] = firstValue;
+            }
+        }
+
+    }
+}
