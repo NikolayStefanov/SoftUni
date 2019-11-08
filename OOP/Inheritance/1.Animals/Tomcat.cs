f969ebaf1932bc9c4892607221bb71eb4da13c3a
NikolayStefanov <54640586+NikolayStefanov@users.noreply.github.com>
2019-11-08 12:27:31 +0200

    Add files via upload

---
+++ OOP/Inheritance/1.Animals/Tomcat.cs
@@ -0,0 +1,14 @@
+﻿namespace Animals
+{
+    public class Tomcat : Cat
+    {
+        private const string TheGender = "Male";
+        public Tomcat(string name, int age) : base(name, age, TheGender)
+        {
+        }
+        public override string ProduceSound()
+        {
+            return "MEOW";
+        }
+    }
+}
