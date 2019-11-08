39174fc6a7a9e4b5d2417a08bf775d6117b3bf7a
NikolayStefanov <54640586+NikolayStefanov@users.noreply.github.com>
2019-11-08 12:19:08 +0200

    Add files via upload

--- OOP/WorkingWithAbstraction/2.CarsSalesman/Program.cs
+++ OOP/WorkingWithAbstraction/2.CarsSalesman/Program.cs
@@ -1,1 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
+namespace P02_CarsSalesman
+{
+    class CarSalesman
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+            List<Engine> engines = new List<Engine>();
+            int engineCount = int.Parse(Console.ReadLine());
+            CreateEngines(engines, engineCount);
+           
+            int carCount = int.Parse(Console.ReadLine());
+            CreateCars(cars, carCount, engines);
+           
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car);
+            }
+        }
+
+        private static void CreateCars(List<Car> cars, int carCount, List<Engine> engines)
+        {
+            for (int i = 0; i < carCount; i++)
+            {
+                string[] parameters = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                string model = parameters[0];
+                string engineModel = parameters[1];
+                Engine engine = engines.FirstOrDefault(x => x.Model == engineModel);
+
+                int weight = -1;
+
+                if (parameters.Length == 3 && int.TryParse(parameters[2], out weight))
+                {
+                    cars.Add(new Car(model, engine, weight));
+                }
+                else if (parameters.Length == 3)
+                {
+                    string color = parameters[2];
+                    cars.Add(new Car(model, engine, color));
+                }
+                else if (parameters.Length == 4)
+                {
+                    string color = parameters[3];
+                    cars.Add(new Car(model, engine, int.Parse(parameters[2]), color));
+                }
+                else
+                {
+                    cars.Add(new Car(model, engine));
+                }
+            }
+        }
+
+        private static void CreateEngines(List<Engine> engines, int engineCount)
+        {
+            for (int i = 0; i < engineCount; i++)
+            {
+                string[] parameters = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                string model = parameters[0];
+                int power = int.Parse(parameters[1]);
+
+                int displacement = -1;
+
+                if (parameters.Length == 3 && int.TryParse(parameters[2], out displacement))
+                {
+                    engines.Add(new Engine(model, power, displacement));
+                }
+                else if (parameters.Length == 3)
+                {
+                    string efficiency = parameters[2];
+                    engines.Add(new Engine(model, power, efficiency));
+                }
+                else if (parameters.Length == 4)
+                {
+                    string efficiency = parameters[3];
+                    engines.Add(new Engine(model, power, int.Parse(parameters[2]), efficiency));
+                }
+                else
+                {
+                    engines.Add(new Engine(model, power));
+                }
+            }
+        }
+    }
+}
