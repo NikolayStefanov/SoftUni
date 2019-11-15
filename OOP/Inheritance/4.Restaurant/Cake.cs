e996ee0aeddeb7a93e987ec4c37aabd73d4231ee
NikolayStefanov <54640586+NikolayStefanov@users.noreply.github.com>
2019-11-15 17:00:54 +0200

    Add files via upload

---
+++ OOP/Inheritance/4.Restaurant/Cake.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        public Cake(string name) : base(name, 5, 250, 1000)
+        {
+            this.CakePrice = 5;
+        }
+        public double CakePrice { get; set; }
+
+    }
+}
--- OOP/Inheritance/4.Restaurant/StartUp.cs
+++ OOP/Inheritance/4.Restaurant/StartUp.cs
@@ -1,1 +1,10 @@
+﻿namespace Restaurant
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
 
+        }
+    }
+}
