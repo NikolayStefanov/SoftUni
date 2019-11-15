5fedaa7d7acf68ff929dfc6d3fba409e933920c7
NikolayStefanov <54640586+NikolayStefanov@users.noreply.github.com>
2019-11-15 16:56:53 +0200

    Add files via upload

--- OOP/Inheritance/Person/StartUp.cs
+++ OOP/Inheritance/Person/StartUp.cs
@@ -1,1 +1,16 @@
+﻿using System;
 
+namespace Person
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());
+            var person = new Child(name, age);
+
+            Console.WriteLine(person.ToString());
+        }
+    }
+}
