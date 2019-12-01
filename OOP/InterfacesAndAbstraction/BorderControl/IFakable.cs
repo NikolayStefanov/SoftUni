336a8af12fc4f40c7bd49b8465a8b03d6305825b
NikolayStefanov <54640586+NikolayStefanov@users.noreply.github.com>
2019-12-01 22:19:54 +0200

    Add files via upload

---
+++ OOP/InterfacesAndAbstraction/BorderControl/IFakable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _5.BorderControl
+{
+    interface IFakable
+    {
+        public string Name { get; set; }
+        public string ID { get; set; }
+    }
+}
