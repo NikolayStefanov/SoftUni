0b59bafead543b190cb3063f01ee447e81e90806
NikolayStefanov <54640586+NikolayStefanov@users.noreply.github.com>
2019-12-01 22:18:19 +0200

    Add files via upload

---
+++ OOP/InterfacesAndAbstraction/FerrariExercise/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _3.Ferrari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            var ferrari = new Ferrari(name);
+            Console.WriteLine(ferrari);
+        }
+    }
+}
