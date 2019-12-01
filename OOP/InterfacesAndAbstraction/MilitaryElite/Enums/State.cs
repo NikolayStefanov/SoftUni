2c959ec175e6cf895f1fb6ad6f7ae7cd16bec9cc
NikolayStefanov <54640586+NikolayStefanov@users.noreply.github.com>
2019-12-01 22:31:13 +0200

    Add files via upload

---
+++ OOP/InterfacesAndAbstraction/MilitaryElite/Enums/State.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _8.MilitaryElite.Enums
+{
+    public enum State
+    {
+        inProgress =1,
+        Finished = 2 
+    }
+}
