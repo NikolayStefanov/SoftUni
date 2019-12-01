b2ad1715889bdfbef759ee6417c0a59c1060252b
NikolayStefanov <54640586+NikolayStefanov@users.noreply.github.com>
2019-12-01 22:19:02 +0200

    Add files via upload

---
+++ OOP/InterfacesAndAbstraction/Telephony/IBrowserable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _4.Telephony
+{
+    interface IBrowserable
+    {
+        public string Browsing(string webSite);
+    }
+}
