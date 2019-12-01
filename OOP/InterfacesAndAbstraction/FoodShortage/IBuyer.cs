59d0d08a6b1a6a7eaff48cc123c9cde330bc9275
NikolayStefanov <54640586+NikolayStefanov@users.noreply.github.com>
2019-12-01 22:24:44 +0200

    Add files via upload

---
+++ OOP/InterfacesAndAbstraction/FoodShortage/IBuyer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _7.Food_Shortage
+{
+    public interface IBuyer
+    {
+        public int Food { get;}
+        public string Name { get; }
+        public void BuyFood();
+    }
+}
