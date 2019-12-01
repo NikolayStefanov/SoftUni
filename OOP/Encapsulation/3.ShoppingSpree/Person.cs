01fc25a4d4810dbe37bb878bced55c8c5f46a157
NikolayStefanov <54640586+NikolayStefanov@users.noreply.github.com>
2019-12-01 22:35:04 +0200

    Add files via upload

---
+++ OOP/Encapsulation/3.ShoppingSpree/Person.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShoppingSpree
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        public Person(string name, decimal money)
+        {
+            this.Products = new List<string>();
+            this.Name = name;
+            this.Money = money;
+        }
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if (value != string.Empty && value != null && value != " ")
+                {
+                    this.name = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+            }
+        }
+
+        public decimal Money
+        {
+            get
+            {
+                return this.money;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    this.money = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+            }
+        }
+
+        public List<string> Products { get; }
+
+        public void Add(string product)
+        {
+            this.Products.Add(product);
+        }
+    }
+}
