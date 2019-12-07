a40cd4de5fb8c466f9fc9e8a0b67d55f2b290bd1
NikolayStefanov <54640586+NikolayStefanov@users.noreply.github.com>
2019-12-07 15:24:38 +0200

    Add files via upload

--- OOP/ExamTries/07.December/StartUp.cs
+++ OOP/ExamTries/07.December/StartUp.cs
@@ -1,1 +1,17 @@
+﻿using System;
 
+namespace MXGP
+{
+    using Models.Motorcycles;
+    using MXGP.Core.Contracts;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            //TODO Add IEngine
+            var engine = new IEngine();
+            engine.Run();
+        }
+    }
+}
