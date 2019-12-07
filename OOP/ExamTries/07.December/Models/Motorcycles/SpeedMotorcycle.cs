66eec2e3bc22c81d67211c2d526fd414ab3f15d5
NikolayStefanov <54640586+NikolayStefanov@users.noreply.github.com>
2019-12-07 15:26:29 +0200

    Add files via upload

---
+++ OOP/ExamTries/07.December/Models/Motorcycles/SpeedMotorcycle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MXGP.Models.Motorcycles
+{
+    public class SpeedMotorcycle : Motorcycle
+    {
+        private const double cubicSm = 125;
+        private const int minHP = 50;
+        private const int maxHP = 69;
+        private int horsePower;
+        public SpeedMotorcycle(string model, int horsePower) : base(model, horsePower, cubicSm)
+        {
+            this.HorsePower = horsePower;
+        }
+        public override int HorsePower 
+        { 
+            get => this.horsePower;
+            protected set 
+            {
+                if (value < minHP || value > maxHP)
+                {
+                    throw new ArgumentException($"Invalid horse power: {value}.");
+                }
+                this.horsePower = value;
+            }
+        }
+    }
+}
