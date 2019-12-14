3f1700c4ed7188be51a15d17f1a6149bd253c3ec
NikolayStefanov <54640586+NikolayStefanov@users.noreply.github.com>
2019-12-14 20:06:36 +0200

    Add files via upload

--- OOP/ExamTries/15 August/SpaceStation/Models/Astronauts/Astronaut.cs
+++ OOP/ExamTries/15 August/SpaceStation/Models/Astronauts/Astronaut.cs
@@ -1,1 +1,62 @@
+﻿using SpaceStation.Models.Astronauts.Contracts;
+using SpaceStation.Models.Bags;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
+namespace SpaceStation.Models.Astronauts
+{
+    public abstract class Astronaut : IAstronaut
+    {
+        private string name;
+        private double oxygen;
+        public Astronaut(string name, double oxygen)
+        {
+            this.Name = name;
+            this.Oxygen = oxygen;
+            this.Bag = new Backpack();
+        }
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Astronaut name cannot be null or empty.");
+                }
+                this.name = value;
+            }
+        }
+
+        public double Oxygen
+        {
+            get => this.oxygen;
+            protected set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Cannot create Astronaut with negative oxygen!");
+                }
+                this.oxygen = value;
+            }
+        }
+
+        public bool CanBreath => this.Oxygen > 0;
+
+        public IBag Bag { get;}
+
+        public virtual void Breath()
+        {
+            var oxygenAfterBreath = this.Oxygen - 10;
+            if (oxygenAfterBreath < 0)
+            {
+                this.Oxygen = 0;
+            }
+            else
+            {
+                this.Oxygen = oxygenAfterBreath;
+            }
+        }
+    }
+}
---
+++ OOP/ExamTries/15 August/SpaceStation/Models/Astronauts/Biologist.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceStation.Models.Astronauts
+{
+    public class Biologist : Astronaut
+    {
+        private const double initialOxygen = 70;
+        public Biologist(string name) : base(name, initialOxygen)
+        {
+        }
+        public override void Breath()
+        {
+            var oxygenAfterBreath = this.Oxygen - 5;
+            if (oxygenAfterBreath < 0)
+            {
+                 this.Oxygen = 0;
+            }
+            else
+            {
+                this.Oxygen = oxygenAfterBreath;
+            }
+        }
+    }
+}
