7e2122d9fb2d8c66836428215f790e9c8d932b42
Nikolay <sennawalker777@gmail.com>
2020-04-03 11:58:14 +0300

    .

--- JS Applications/Exam Preparation/Trekking Zone/controllers/index.js
+++
@@ -1,9 +0,0 @@
-import user from "./user.js";
-import trek from "./trek.js";
-import home from "./home.js";
-
-export default {
-    user,
-    trek,
-    home
-};
--- JS Applications/Exam Preparation/Trekking Zone/models/index.js
+++
@@ -1,7 +0,0 @@
-import user from "./user.js";
-import trek from "./trek.js";
-
-export default{
-    user,
-    trek
-}
